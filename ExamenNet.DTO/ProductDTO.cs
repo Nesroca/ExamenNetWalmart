7c1b8b7cf97425f0b24852682c84e660ade93723
Nesroca <nesroca@hotmail.com>
2020-01-10 18:30:19 -0600

    Creacion del proyecto de modelo de datos

---
+++ ExamenNet.DTO/ProductDTO.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+
+
+namespace ExamenNet.DTO
+{
+    [Table("products")]
+    public class ProductDTO
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string sku { get; set; }
+        public string description { get; set; }
+    }
+}
---
+++ ExamenNet.DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace ExamenNet.DTO
+{
+    [Table("users")]
+    public class UserDTO
+    {
+        [Key]
+        public int id { get; set; }
+        public string user_name { get; set; }
+        public string pwd { get; set; }
+    }
+}
