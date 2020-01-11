02a431b5e83780df64698dba613f185121abc29f
Nesroca <nesroca@hotmail.com>
2020-01-10 19:05:24 -0600

    Se agrega capa de trabajo con las instancias de los objetos que se necesitaran para la base de datos postgres.

---
+++ ExamenNet.UnitOfWork/ExamenUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using ExamenNet.Business.Implementacion;
+using ExamenNet.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamenNet.UnitOfWork
+{
+    public class ExamenUnitOfWork : IExamenDBUnitOfWork
+    {
+
+        public ExamenUnitOfWork(string connectionString)
+        {
+            Product = new Product(connectionString);
+            Login = new Login(connectionString);
+        }
+
+        public IProduct Product { get; private set; }
+        public ILogin Login { get; private set; }
+    }
+}
---
+++ ExamenNet.UnitOfWork/IExamenDBUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using ExamenNet.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamenNet.UnitOfWork
+{
+    public interface IExamenDBUnitOfWork
+    {
+        IProduct Product { get; }
+        ILogin Login { get; }
+    }
+}
